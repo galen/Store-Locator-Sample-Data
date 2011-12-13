0d49de841d9def043c90d1ac770659a5112a23f0
Galen <galenjr@gmail.com>
2011-12-13 02:12:53 -0800

    Add enum to country

--- pizza_us.sql
+++ pizza_us.sql
@@ -6,7 +6,7 @@ CREATE TABLE `pizza` (
   `address` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `city` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `state` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `country` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `country` enum('US','AF','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BA','BW','BR','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CG','CK','CR','HR','CU','CY','CZ','DK','DJ','DM','DO','TP','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','GF','PF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GP','GU','GT','GN','GW','GY','HT','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IL','IT','CI','JM','JP','JO','KZ','KE','KI','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MQ','MR','MU','YT','MX','FM','MD','MC','MN','MS','MA','MZ','MM','NA','NR','NP','NL','AN','NC','NZ','NI','NE','NG','NU','NF','KP','MP','NO','OM','PK','PW','PA','PG','PY','PE','PH','PL','PT','PR','QA','RE','RO','RU','RW','SH','KN','LC','PM','VC','SM','ST','SA','SN','SC','SL','SG','SK','SI','SB','SO','ZA','KR','ES','LK','SD','SR','SZ','SE','CH','SY','TW','TJ','TZ','TH','TG','TK','TO','TT','TN','TR','TM','TC','TV','UG','UA','AE','UK','UY','UZ','VU','VA','VE','VN','VG','VI','WF','WS','YE','YU','ZR','ZM','ZW') COLLATE utf8_unicode_ci DEFAULT NULL,
   `lat` float(10,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
   PRIMARY KEY (`id`)
